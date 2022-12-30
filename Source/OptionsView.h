db94a5e0454ae076ff88d28deb4812c78bb37a11
Teknomancer <1089717+Teknomancer@users.noreply.github.com>
2022-12-30 10:32:50 +0530

    Source: Added SPDX license identifiers to all source files (#3).

--- Source/OptionsView.h
+++ Source/OptionsView.h
@@ -1,19 +1,6 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//  This file is part of BeConverter.
-//
-//  BeConverter is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  BeConverter is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with BeConverter.  If not, see <http://www.gnu.org/licenses/>.
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2001 Ramshankar (aka Teknomancer).
+// All rights reserved.
 
 #ifndef _OPTIONS_VIEW_H
 #define _OPTIONS_VIEW_H
