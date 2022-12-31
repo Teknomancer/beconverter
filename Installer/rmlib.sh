bc1fd26d71f244684abdac36d9478a27d0f320b4
Oscar Lesta <oscar.lesta@gmail.com>
2022-12-30 21:51:09 -0300

    Cleanup: removal of old binaries from branch "master".
    The binaries can still be accessed via the "v2016_01_01-legacy" tag,
    or from the "legacy" branch, if there's a need for them.

--- Installer/rmlib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Script that removes libBeConverter.so from ~/config/lib.
-# As BeConverter from version 1.2 upwards puts the lib
-# in the "lib" folder of the app itself.
-# BeConverter (c) 2002 Ramshankar.
-
-rm /boot/home/config/lib/libBeConverter.so
